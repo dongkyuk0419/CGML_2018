0a053d5b24043668e48338b6479257af6012b36f
dongkyuk0419 <dongkyuk0419@gmail.com>
2018-10-03 14:33:01 -0400

    add comments

--- Week4/CheckPoints/week4_cifar_10.py
+++ Week4/CheckPoints/week4_cifar_10.py
@@ -11,11 +11,12 @@ import tensorflow as tf
 from keras import optimizers, regularizers
 from keras.datasets import cifar10
 from keras.models import Model
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Activation, Input
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers import BatchNormalization, Activation, Input
 from keras.callbacks import LearningRateScheduler
 
 # Parameters
-r = 0.9 # Ratio of training data otu of training + validation
+r = 0.9 # Ratio of training data to (training + validation)
 batch_size = 128
 epochs = 200
 numclass = 10
@@ -37,14 +38,14 @@ def lr_adaptive(self,epoch):
 # Data Generation
 class Data(object):
     def __init__(self,r,numclass): # N is number of training data that will be used
-        (self.xtemp,self.ytemp),(self.X_test,self.Y_test) = cifar10.load_data()
-        mask = np.full(self.xtemp.shape[0],True)
-        mask[np.random.choice(self.xtemp.shape[0], int(self.xtemp.shape[0]*(1-r)), replace=False)] = False
+        (xtemp,ytemp),(self.X_test,self.Y_test) = cifar10.load_data()
+        mask = np.full(xtemp.shape[0],True)
+        mask[np.random.choice(xtemp.shape[0],int(xtemp.shape[0]*(1-r)),replace=False)] = False
         self.numclass = numclass
-        self.X_train = self.xtemp[mask].astype('float32')
-        self.Y_train = self.ytemp[mask]
-        self.X_val = self.xtemp[~mask].astype('float32')
-        self.Y_val = self.ytemp[~mask]
+        self.X_train = xtemp[mask].astype('float32')
+        self.Y_train = ytemp[mask]
+        self.X_val = xtemp[~mask].astype('float32')
+        self.Y_val = ytemp[~mask]
         self.X_test = self.X_test.astype('float32')
         self.Y_test = self.Y_test
         self.X_train = self.normalize(self.X_train)
@@ -56,8 +57,9 @@ class Data(object):
 
     def normalize(self,X):
         X /= 255
+        # These values are from https://github.com/kuangliu/pytorch-cifar/issues/19
         mu = [0.4914,0.4822,0.4465]
-        std = [0.2023,0.1994,0.2010] # These values are from https://github.com/kuangliu/pytorch-cifar/issues/19
+        std = [0.2023,0.1994,0.2010]
         for i in range(0,2):
             X[:,:,:,i] = (X[:,:,:,i]-mu[i])/std[i]
         return X
@@ -71,8 +73,10 @@ class My_Model(object):
 
     def convconv(self,filters,kernel_size,strides):
         return Conv2D(filters,kernel_size,strides =strides,padding='same',activation='relu')
-
-    def residual_block(self,input,filter): #This is from https://arxiv.org/pdf/1603.05027.pdf Identity Mappings in Deep Residual Networks
+    #This is from https://arxiv.org/pdf/1603.05027.pdf Identity Mappings in Deep Residual Networks
+    #Second branch is motivated by https://arxiv.org/pdf/1705.07485.pdf Shake-Shake regularization
+    #The below function is ultimately unused, however I left this here for future usage.
+    def residual_block(self,input,filter):
         x = BatchNormalization()(input)
         x = Activation('relu')(x)
         x = self.convconv(filter,[3,3],1)(x)
@@ -88,6 +92,7 @@ class My_Model(object):
         y = self.convconv(filter,[3,3],1)(y)
         return keras.layers.add([input,x,y])
 
+    #This is a combination of residual block without the identity connection.
     def all_conv_block(self,filters,dropout,input):
         x = self.convconv(filters,[3,3],1)(input)
         x = BatchNormalization()(x)
@@ -99,31 +104,16 @@ class My_Model(object):
 
     def build_model(self):
         input = Input(shape = self.data.X_train.shape[1:])
-        x = self.all_conv_block(32,0.25,input)
+        x = self.all_conv_block(32,0.25,input)        
         x = self.all_conv_block(64,0.35,x)
         x = self.all_conv_block(128,0.45,x)
         x = self.all_conv_block(256,0.35,x)
-#        x = self.convconv(64,[7,7],2)(input)
-#        x = MaxPooling2D(3,strides = 2)(x)
-#        for i in range(0,2):
-#            x = self.residual_block(x,64)
-#        x = self.convconv(128,[3,3],2)(x)
-#        for i in range(0,2):
-#            x = self.residual_block(x,128)
-#        x = self.convconv(256,[3,3],2)(x)
-#        for i in range(0,4):
-#            x = self.residual_block(x,256)        
-#        x = self.convconv(512,[3,3],2)(x)
-#        for i in range(0,2):
-#            x = self.residual_block(x,512)
-#        x = keras.layers.GlobalAveragePooling2D()(x)
         x = Flatten()(x)
         x = Dense(1024,activation = 'relu')(x)
         y = Dense(self.data.numclass,activation='softmax')(x)
         self.model = Model(inputs = input, outputs = y)
 
     def train(self):
-#        self.optim = optimizers.SGD(learning_rate,momentum=0.9,decay=5e-4)
         self.optim = optimizers.Adam(learning_rate)
         self.model.compile(self.optim,'categorical_crossentropy',['accuracy'])
         self.datagen = keras.preprocessing.image.ImageDataGenerator(
@@ -131,10 +121,10 @@ class My_Model(object):
             width_shift_range = 4, height_shift_range = 4
             )
         self.datagen.fit(self.data.X_train)
-        self.model.fit_generator(self.datagen.flow(self.data.X_train, self.data.Y_train,batch_size=self.batch_size),
-            steps_per_epoch=len(self.data.X_train)/self.batch_size,epochs=self.epochs,validation_data = (self.data.X_val,self.data.Y_val),
+        self.model.fit_generator(self.datagen.flow(self.data.X_train,self.data.Y_train,
+            batch_size=self.batch_size),steps_per_epoch=len(self.data.X_train)/self.batch_size,
+            epochs=self.epochs,validation_data = (self.data.X_val,self.data.Y_val),
             callbacks=[LearningRateScheduler(lr_adaptive)],verbose=2)
-#       self.model.fit(self.data.X_train,self.data.Y_train,self.batch_size,self.epochs,validation_data = (self.data.X_val,self.data.Y_val),shuffle=True)
 
 print('This is my cifar_10 case')
 data = Data(r,numclass)
@@ -145,18 +135,22 @@ scores = My_Model.model.evaluate(data.X_test,data.Y_test,verbose=2)
 print('Test loss: ', scores[0])
 print('Test accuracy:',scores[1])
 
-# I started with my MNIST model, it didn't work well
-# I experimented with a deeper version of the MNIST model, and it just lingered at 50%
-# I moved onto residual neural network, https://arxiv.org/pdf/1512.03385.pdf(Deep Residual Learning for Image Recognition), with 
-# identity shortcut https://arxiv.org/pdf/1603.05027.pdf (Identity Mappings in Deep Residual Networks).
-# After 32 epochs, it converges at validation accuracy of 0.6479, and test set accuracy of 0.9967, with best validation accuracy of 0.6513
-# This model overfits, so I added a simple data augmentation scheme that randomly flips horizontal and vertically.
-# With this augmentation method, I reach a validation accuracy of 0.6821, which is slightly better, but not that good.
-# Then I implemented the augmentation method that is close to the above papers, 0.721 and achived with 64 epochs
-# Then I realized that I didn't do normalization on the data, and found values for optimal mean and std online to normalize.
-# The result improved a lot, with epochs 64, now the model reaches 0.8042 validation error
-# I changed to elu. 0.7898 at 30th, I changed back to relu and added rotation_range in the data augmentation, this didn't help
-# I added another branch in residual network. This idea was motivated by https://arxiv.org/pdf/1705.07485.pdf (Shake-Shake regularization)
-# I increased the learning rate from 0.005 to 0.007 because new model was slow. However, this converges to 0.76. I realized my method is just not working out.
-# I heard from someone (joey) that all conv layer works fine, and it does work greatly with only 8 total layers. (I was disappointed), it goes to 0.8214
-# I realized it learns slowly, so I added adaptive learning rate, and added another conv_block.
+# I started with my MNIST model, it didn't work well. Then I experimented with a deeper version
+# of the MNIST model, but it just achieved 50% top-1 accuracy. I moved into residual neural network
+# based on https://arxiv.org/pdf/1512.03385.pdf(Deep Residual Learning for Image Recognition), and 
+# https://arxiv.org/pdf/1603.05027.pdf (Identity Mappings in Deep Residual Networks).
+# After 32 epochs, it converges at validation accuracy of 0.6479, and train set accuracy of 0.9967,
+# with best validation accuracy of 0.6513. This model overfits, so I added a simple data
+# augmentation scheme. With this augmentation method, I reach a validation accuracy of 0.6821,
+# which is slightly better, but not that good. Then I did the data normalization off of a already
+# known values because I didn't want to waste computational time, and result improved a lot to
+# 0.8042 validation error.
+# I tried elu, rotation_range in data augmentation, added another branch in residual network
+# played around with learning rate, but all of these methods didn't really help.
+# Then I discussed with classmates and heard that simple convolutional neural network might work
+# better. This made sense to me because even in the residual network paper, they mention for the
+# deep versions of the model, they started with a simple conv net layers, and then add the residual
+# network, and all the state of the art papers ran the model for 6 weeks, so maybe with my device
+# and time, residual network wasn't effective. So I changed back to a deep 12 layer network with
+# conv-net blocks, with adaptive learning rate, I achieved validation accuracy of 0.9120 and
+# test accuracy of 0.9029.
